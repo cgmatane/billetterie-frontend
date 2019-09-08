fa58aaa673208444deef98998f868bd9278c151c
Loic Hug <h-loic@orange.fr>
2019-09-08 18:43:16 -0400

    taille bouton + relation avec autre page

--- page/accueil.php
+++ page/accueil.php
@@ -7,8 +7,8 @@
     <div class="text-center row justify-content-center">
         <div class="img-thumbnail col-sm-6 col-xs-8 bg-light pb-5">
         <h4 class="text-center mt-3">Choississez votre destination</h4>
-        <button type="button" class="btn btn-success mt-5">Godbout</button><br>
-            <button type="button" class="btn btn-success mt-5">Baie-Comeau</button>
+        <a href="index.php?page=page1"><button type="button" class="btn btn-success mt-5 col-6 " >Godbout</button></a><br>
+            <a href="index.php?page=page1"><button type="button" class="btn btn-success col-6 mt-5" >Baie-Comeau</button></a>
         </div>
     </div>
 </div>
