3073e8f8fa7bc25f5cab296663c4da894414acff
Loic Hug <h-loic@orange.fr>
2019-09-08 17:04:00 -0400

    couleur footer

--- resource/include/footer.php
+++ resource/include/footer.php
@@ -1,8 +1,8 @@
-<footer class="container-fluid fixed-bottom bg-dark">
+<footer class="container-fluid fixed-bottom bg-light">
     <div class="row">
 
         <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
-            <span class="navbar-text text-white">
+            <span class="navbar-text text-dark">
                 Billetterie - STQ
             </span>
         </div>
