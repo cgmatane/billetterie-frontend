22c48441a921fbcd3e51820fb60ee67cf1be1b82
Loic Hug <h-loic@orange.fr>
2019-09-08 18:35:24 -0400

    choix destination responsive

--- page/accueil.php
+++ page/accueil.php
@@ -5,7 +5,7 @@
         </div>
     </div>
     <div class="text-center row justify-content-center">
-        <div class="img-thumbnail col-6 bg-light pb-5">
+        <div class="img-thumbnail col-sm-6 col-xs-8 bg-light pb-5">
         <h4 class="text-center mt-3">Choississez votre destination</h4>
         <button type="button" class="btn btn-success mt-5">Godbout</button><br>
             <button type="button" class="btn btn-success mt-5">Baie-Comeau</button>
