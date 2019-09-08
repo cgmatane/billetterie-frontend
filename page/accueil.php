fb2fa37767ef322c0fe8a28dab2b135b7cf4b3a0
Loic Hug <h-loic@orange.fr>
2019-09-08 18:53:44 -0400

    ajout bouton choisir une autre date

--- page/accueil.php
+++ page/accueil.php
@@ -6,9 +6,12 @@
     </div>
     <div class="text-center row justify-content-center">
         <div class="img-thumbnail col-sm-6 col-xs-8 bg-light pb-5">
-        <h4 class="text-center mt-3">Choississez votre destination</h4>
-        <a href="index.php?page=page1"><button type="button" class="btn btn-success mt-5 col-6 " >Godbout</button></a><br>
-            <a href="index.php?page=page1"><button type="button" class="btn btn-success col-6 mt-5" >Baie-Comeau</button></a>
+        <h4 class="text-center mt-3">Choisissez votre destination</h4>
+        <a href="index.php?page=page1"><button type="button" class="btn btn-success mt-5 col-6 p-3" >Godbout</button></a><br>
+            <a href="index.php?page=page1"><button type="button" class="btn btn-success col-6 mt-5 p-3" >Baie-Comeau</button></a>
         </div>
+    </div>
+    <div class="row text-center justify-content-center">
+        <a><button type="button" class="btn btn-danger m-5 p-3" >Choisir une autre date</button></a>
     </div>
 </div>
