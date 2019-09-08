90a6c5820bba8b28c9745d485df2bf052b7fc4bc
Loic Hug <h-loic@orange.fr>
2019-09-08 16:59:11 -0400

    ajout information du lieux de depart

--- page/accueil.php
+++ page/accueil.php
@@ -1,8 +1,11 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <h1>Acceuil</h1>
-            <a href="index.php?page=page1">page 1</a>
+            <h1 class="text-center text-warning p-4 font-weight-bold">Départ Matane 4 septembre</h1>
+
         </div>
+    </div>
+    <div class="row">
+        <a href="index.php?page=page1">page 1</a>
     </div>
 </div>
