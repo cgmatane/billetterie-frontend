a487cfc633f52837c7cf7e2ebf0da8aa93b0c7e9
Loic Hug <h-loic@orange.fr>
2019-09-08 21:14:10 -0400

    barre d avancement responsive

--- page/page2.php
+++ page/page2.php
@@ -1,7 +1,6 @@
-<img src="resource/image/avancement1.PNG" width="100%">
-<div class="container-fluid">
+
     <div class="row">
-        <div class="col-12">
+        <div class="col-sm-12 col-xs-0">
+            <img src="resource/image/avancement1.PNG" width="100%">
         </div>
     </div>
-</div>
