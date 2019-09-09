84925f3b1a061bc0864f3c571110bea9ed323920
Loic Hug <h-loic@orange.fr>
2019-09-09 14:41:31 -0400

    ajout page reservation_animaux et renommage des pages

--- index.php
+++ index.php
@@ -7,9 +7,10 @@ if (isset($_GET['page']))
 {
     switch($_GET['page'])
     {
-        case "page1": include('page/page1.php'); break;
-        case "page2": include('page/page2.php'); break;
-        case "page3": include('page/page3.php'); break;
+        case "reservation_choix_horaire_depart": include('page/reservation_choix_horaire_depart.php'); break;
+        case "reservation_animaux": include('page/reservation_animaux.php'); break;
+        case "information_animaux": include('page/information_animaux.php'); break;
+        case "reservation_choix_vehicule": include('page/reservation_choix_vehicule.php'); break;
         default: include('page/accueil.php');
     }
 }
--- page/reservation_animaux.php
+++ page/reservation_animaux.php
@@ -2,14 +2,14 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <h1 class="text-center text-warning p-4 font-weight-bold">Avez-vous une voiture ?</h1>
+            <h1 class="text-center text-warning p-4 font-weight-bold">Voyagez-vous avec au moins un animal d'une autre espèce que chien ou chat ?</h1>
         </div>
     </div>
     <div class="row text-center">
-        <a href="index.php?page=page3" class="bg-white rounded-sm col-5 ml-1 mx-auto pb-5 pt-5 display-4"
+        <a href="index.php?page=informations_animaux" class="bg-white rounded-sm col-5 ml-1 mx-auto pb-5 pt-5 display-4"
            style="height: 200px; text-decoration:none;">OUI</a>
-        
-        <a href="index.php?page=page2" class="bg-white rounded-sm col-5 ml-1 mx-auto pb-5 pt-5 display-4"
+
+        <a href="index.php?page=reservation_choix_vehicule" class="bg-white rounded-sm col-5 ml-1 mx-auto pb-5 pt-5 display-4"
            style="height: 200px; text-decoration:none;">NON</a>
     </div>
 
