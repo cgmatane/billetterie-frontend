0c25808bb2645818b43bdc817b92e128eae2b15b
Loic Hug <h-loic@orange.fr>
2019-09-08 21:37:53 -0400

    ajout choix heure de départ

--- page/page2.php
+++ page/page2.php
@@ -4,3 +4,16 @@
         <img src="resource/image/avancement1.PNG" width="100%">
     </div>
 </div>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center text-warning font-weight-bold mt-4">Choisissez une heure de départ</h1>
+        </div>
+    </div>
+    <div class="row text-center">
+        <a href="index.php?page=accueil" class="img-thumbnail mx-auto p-5 m-5">11h30</a>
+        <a href="index.php?page=accueil" class="img-thumbnail mx-auto p-5 m-5">13h00</a>
+        <a href="index.php?page=accueil" class="img-thumbnail mx-auto p-5 m-5">14h30</a>
+        <a href="index.php?page=accueil" class="img-thumbnail mx-auto p-5 m-5">17h00</a>
+    </div>
+</div>
