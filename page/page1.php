3e27661e57bb9c3c4270ddc8fe0e0955a43473b3
Loic Hug <h-loic@orange.fr>
2019-09-08 20:04:16 -0400

    ajout bouton retour

--- page/page1.php
+++ page/page1.php
@@ -5,8 +5,15 @@
         </div>
     </div>
     <div class="row text-center">
-        <a href="index.php?page=accueil" class="img-thumbnail col-5 mr-1 mx-auto pb-5 pt-5 display-4" style="height: 200px;">OUI</a>
-        <a href="index.php?page=accueil" class="img-thumbnail col-5 ml-1 mx-auto pb-5 pt-5 display-4"  style="height: 200px;">NON</a>
+        <a href="index.php?page=accueil" class="img-thumbnail col-5 mr-1 mx-auto pb-5 pt-5 display-4"
+           style="height: 200px;">OUI</a>
+        <a href="index.php?page=accueil" class="img-thumbnail col-5 ml-1 mx-auto pb-5 pt-5 display-4"
+           style="height: 200px;">NON</a>
+    </div>
+
+    <div class="row text-center justify-content-center">
+        <a href="index.php?page=accueil"><button type="button" class="btn btn-danger m-5 p-5">
+                retour</button></a>
     </div>
 </div>
 
