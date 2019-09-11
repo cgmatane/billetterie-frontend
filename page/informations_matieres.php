42b79e315d1a81e13dd4821171e30b7aaca49007
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2019-09-11 08:10:50 -0400

    Page informations matières dangereuses

--- page/informations_matieres.php
+++ page/informations_matieres.php
@@ -1,1 +1,18 @@
-EN COURS DE DEVELOPPEMENT
+<body id="top" style="background-color: #004882">
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center text-warning font-weight-bold">Vous avez des matières dangereuses ?</h1>
+        </div>
+    </div>
+    <div class="text-center row justify-content-center">
+        <div class="img-thumbnail col-sm-6 col-xs-8 bg-light pb-5">
+            <p>Afin de garantir la sécurité des passagers à bord du traversier,
+                il est interdit de transporter des matières dangereuses.</p>
+        </div>
+    </div>
+    <div class="row text-center justify-content-center">
+        <a href="index.php"><button type="button" class="btn btn-danger m-5 px-5 py-lg-5 py-xs-2">
+                retour</button></a>
+    </div>
+</div>
