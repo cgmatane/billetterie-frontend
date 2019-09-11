10262041d81f639db90e58c02f7170939e98d54a
Loic Hug <h-loic@orange.fr>
2019-09-11 13:54:38 -0400

    ajout heure de départ a l accueil

--- page/accueil.php
+++ page/accueil.php
@@ -7,13 +7,17 @@
     </div>
     <div class="text-center row justify-content-center">
         <div class="img-thumbnail col-sm-6 col-xs-8 bg-light pb-5">
-        <h4 class="text-center mt-3">Choisissez votre destination</h4>
-        <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-success mt-5 col-6 p-3" >Godbout</button></a><br>
-            <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-success col-6 mt-5 p-3" >Baie-Comeau</button></a>
+        <h4 class="text-center mt-3">Choisissez votre destination/heure de départ</h4>
+        <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-success mt-5 col-6 p-3" >Godbout : 10h00</button></a><br>
+            <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-success mt-5 col-6 p-3" >Godbout : 13h30</button></a><br>
+            <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-success mt-5 col-6 p-3" >Godbout : 16h00</button></a><br>
+            <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-success col-6 mt-5 p-3" >Baie-Comeau : 10h45</button></a>
+            <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-success col-6 mt-5 p-3" >Baie-Comeau : 13h00</button></a>
+            <a href="index.php?page=reservation_choix_vehicule"><button type="button" class="btn btn-success col-6 mt-5 p-3" >Baie-Comeau : 15h30</button></a>
         </div>
     </div>
-    <div class="row text-center justify-content-center">
-        <a href="index.php?page=choix_date"><button type="button" class="btn btn-info mt-5 mr-4 p-3" >Choisir une autre date</button></a>
-        <a href="index.php?page=choix_depart"><button type="button" class="btn btn-info mt-5 ml-4 p-3" >Choisir un autre départ</button></a>
+    <div class="row text-center justify-content-around">
+        <a href="index.php?page=choix_date"><button type="button" class="btn btn-info mt-5 p-3 mb-5" >Choisir une autre date</button></a>
+        <a href="index.php?page=choix_depart"><button type="button" class="btn btn-info mt-5 p-3 mb-5" >Choisir un autre départ</button></a>
     </div>
 </div>
